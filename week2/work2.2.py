33dcfcc8cce6eda6a905df529185afc9db16e28f
Nitirat Poonchai <nitirat.p@kkumail.com>
2021-02-11 16:06:58 +0700

    week2

---
+++ week2/work2.2.py
@@ -0,0 +1,10 @@
+days=int (input('INPUT NUMBER OF DAYS:'))
+
+hour = days*24
+print('DAYS --> hour :',hour)
+
+minutes= hour*60
+print('DAYS --> minutes :',minutes)
+
+seconds= minutes*60
+print('DAYS --> seconds :',seconds)
---
+++ week2/work2.3.py
@@ -0,0 +1,23 @@
+print('ADD TO BASKET PROGRAME')
+
+cargo=[]
+
+p1=input('Pick your product1: ') 
+cargo.append(p1)
+p2=input('Pick your product2: ') 
+cargo.append(p2)  
+p3=input('Pick your product3: ') 
+cargo.append(p3)
+p4=input('Pick your product4: ') 
+cargo.append(p4)
+p5=input('Pick your product5: ') 
+cargo.append(p5)
+
+print('\nYour product')
+print('price1:',cargo[0])
+print('price2:',cargo[1])
+print('price3:',cargo[2])
+print('price4:',cargo[3])
+print('price5:',cargo[4])
+
+
